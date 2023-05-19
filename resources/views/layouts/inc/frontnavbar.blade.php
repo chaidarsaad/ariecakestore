07f015b5fdc1ee8405f66dbd5bea71a59389dd75
Chaidar Saad <chaidarsaad55@gmail.com>
2023-05-19 23:51:24 +0700

    Update frontnavbar.blade.php

--- resources/views/layouts/inc/frontnavbar.blade.php
+++ resources/views/layouts/inc/frontnavbar.blade.php
@@ -61,6 +61,11 @@
                     </a>
                 </li>
                 <li>
+                    <a class="dropdown-item" href="{{ url('cek-ongkir') }}">
+                        Cek Ongkos Kirim
+                    </a>
+                </li>
+                <li>
                     <a class="dropdown-item" href="{{ route('logout') }}"
                     onclick="event.preventDefault(); document.getElementById('logout-form').submit();">
                         {{ __('Logout') }}
